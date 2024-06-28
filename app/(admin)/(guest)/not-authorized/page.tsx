8b77b6de9137fb24761ea9189b4ffc3ece2a181c
Mykola Lebid <realkolos1@gmail.com>
2024-06-28 21:22:06 +0300

    not authorized background

--- app/(admin)/(guest)/not-authorized/page.tsx
+++ app/(admin)/(guest)/not-authorized/page.tsx
@@ -6,7 +6,7 @@ import { Logout } from "./_components";
 
 export default function NotAllowedPage() {
   return (
-    <section className="overflow-y-auto p-4">
+    <section className="overflow-y-auto p-4 bg-neutral rounded">
       <div className="prose mx-auto">
         <h2 className="text-center">Sorry!</h2>
         <h3 className="text-center">You account is not authorized yet</h3>
